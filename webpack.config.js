13f745caff5be810741eaec8ed91d79596a2c246
Lip Wang <lip.wang@ringcentral.com>
2021-08-03 09:24:54 -0400

    add webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    library: {
+      name: "easy-pkce",
+      type: "umd",
+    },
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  }
+};
