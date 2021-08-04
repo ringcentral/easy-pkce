fa2aeba9c17ecfe419c24c60c73cec0dfb5e308a
Lip Wang <lip.wang@ringcentral.com>
2021-08-04 14:54:42 -0400

    add namespace @ringcentral for lib

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     library: {
-      name: "easy-pkce",
+      name: "@ringcentral/easy-pkce",
       type: "umd",
     },
   },
